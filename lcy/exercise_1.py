862ed49bbf3678835b590002e72ecebfdd6984dc
ge69zaq <ge69zaq@gitlab.lrz.de>
2019-11-01 15:13:27 +0100

    1st exercise

---
+++ lcy/exercise_1.py
@@ -0,0 +1,30 @@
+import random
+
+lowerBound = 1
+upperBound = 100
+
+number = random.randint(lowerBound, upperBound)
+numUserStr = ""
+numUser = lowerBound - 1
+print("Guess Game starts\npress 'q' to quit\n")
+
+while numUser != number:
+    numUserStr = input("Enter your guess between {} and {}: \n".format(lowerBound, upperBound))
+    try:
+        numUser = int(numUserStr)
+        if numUser < number:
+            print("too low\n")
+        elif numUser > number:
+            print("too high\n")
+        else:
+            print("just right!\n")
+            break
+    except ValueError:
+        if numUserStr == "q":
+            print("you gave up")
+            break
+        else:
+            print("invalid input")
+    finally:
+        pass
+
