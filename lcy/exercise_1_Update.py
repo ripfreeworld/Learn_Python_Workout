15cc7a2e9b6fb7acfacd9b7f9ad7b52781917fb0
ge69zaq <ge69zaq@gitlab.lrz.de>
2019-11-10 20:48:16 +0100

    my best e1_update

---
+++ lcy/exercise_1_Update.py
@@ -0,0 +1,108 @@
+import random
+
+
+def Guessnum(lBound, uBound):
+    numUserStr = ""
+    numUser = -1        # assign a number out of range to the default guess 
+    attempt = 0
+    number = random.randint(lBound, uBound)  
+
+    while numUser != number:
+        numUserStr = input("Enter your guess between {} and {}: \n".format(lBound, uBound))
+        if numUserStr == "q":
+            print("you gave up")
+            break
+        else:
+            try:
+                numUser = int(numUserStr)
+                if numUser < number:
+                    print("too low\n")
+                elif numUser > number:
+                    print("too high\n")
+                else:
+                    print("just right!\n")
+                    break
+                attempt += 1
+            except ValueError:
+                    print("invalid input")
+            finally:
+                if attempt >= limitChance:
+                    print ("You didn't guess in time. Game Over.\n")
+                    break
+
+
+def GuessFruit(Fruitnum):
+    numUser = -1        # assign a number out of range to the default guess 
+    attempt = 0
+
+    while numUser != Fruitnum:
+        getInput = GetFruit()
+        if type(getInput) == str:
+            break
+        else:
+            numUser = getInput
+            if numUser < Fruitnum:
+                print("Please choose a later word!\n")
+            elif numUser > Fruitnum:
+                print("Please choose an earlier word!\n")
+            else:
+                print("just right!\n")
+                break
+            attempt += 1
+            if attempt >= limitChance:
+                print ("You didn't guess in time. Game Over.\n")
+                break
+
+
+def GetFruit():
+    userInput = input(f"please guess a fruit\n")
+    if userInput == "q":
+        return userInput
+    else:
+        try:
+            fruitnum = textDict[userInput]
+        except:
+            print("This fruit is not recorded. Please guess another fruit.")
+            GetFruit()
+        finally:
+            return fruitnum
+
+## main()            
+print("Guess Game starts\npress 'q' to quit\n")
+
+limitChance = 5     # to define how many chances the user is able to guess
+attempt = 0
+print(f"You have {limitChance} chances to guess\n")
+
+gamemode = input("guess a number (Press 1) or a fruit name in dictionary (Press 2)\n")
+
+if gamemode == "1":
+    print("number mode")
+    lowerBound = 1
+    upperBound = 100
+    Guessnum(lowerBound, upperBound)
+
+elif gamemode == "2":
+    print("fruit mode")
+    i = 1
+    textArray = ["apple", "banana", "cherry", "grape", "orange", "pear", "pineapple", "strawberry", "watermelon"]
+    textDict = {}
+    for i in range(1, len(textArray)):
+        textDict[textArray[i]] = i
+    # textDict = {        # if some keys duplicate, the later one would substitute the former one
+    #     "apple": 1,
+    #     "banana": 2,
+    #     "cherry": 3,
+    #     "grape": 4, 
+    #     "orange": 5,
+    #     "pear": 6,
+    #     "pineapple": 7,
+    #     "strawberry": 8,
+    #     "watermelon": 9
+    # }
+    lowerBound = 1
+    upperBound = len(textDict)
+    GuessFruit(random.randint(lowerBound, upperBound))
+else:
+    print("invalid choice!")
+
