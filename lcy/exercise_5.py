db216a4d1811b9067c74d28eafadf6680fbcb481
ge69zaq <ge69zaq@gitlab.lrz.de>
2020-01-03 16:46:10 +0100

    exercise 5 additional

--- lcy/exercise_5.py
+++ lcy/exercise_5.py
@@ -1,4 +1,5 @@
 # Flip a dictionary
+# visualization view: http://pythontutor.com/visualize.html#mode=display
 
 d = {'a':1, 'b':2, 'c':3}
 
@@ -9,3 +10,14 @@ flipped_d = { value : key
               # Even those that return 2-element tuples, such as dict.items.
               
 print(flipped_d)
+
+# Another example of comprehension:
+squares = [i * i for i in range(10)]
+# congruent:
+squares = []
+for i in range(10):
+    squares.append(i * i)
+
+# nested loops create a Cartesian Product of the two sequences
+# https://stackoverflow.com/questions/18649884/python-list-comprehension-for-loops
+
